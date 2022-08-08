08ea17c236c2a12853050d679d1ebc29e41a8856
Kyle Scott <kyscott@umich.edu>
2022-08-08 19:32:46 -0400

    async loop through endpoints

--- src/test.ts
+++ src/test.ts
@@ -26,75 +26,79 @@ interface Output {
 }
 
 export const testResponseTime = async (): Promise<void> => {
-  const endpoint = endpoints[0]!;
-  const provider = new ethers.providers.JsonRpcProvider(endpoint);
-  const gasPrice = await provider.getGasPrice();
-  const env = load({
-    MNEMONIC: String,
-  });
-  const wallet = ethers.Wallet.fromMnemonic(env.MNEMONIC);
-  const signer = wallet.connect(provider);
-
   let output: Output[] = [];
-  let outputData: Data[] = [];
+  await Promise.all(
+    endpoints.map(async (endpoint) => {
+      const provider = new ethers.providers.JsonRpcProvider(endpoint);
+      const gasPrice = await provider.getGasPrice();
+      const env = load({
+        MNEMONIC: String,
+      });
+      const wallet = ethers.Wallet.fromMnemonic(env.MNEMONIC);
+      const signer = wallet.connect(provider);
 
-  for (let awaitMethod in AwaitMethod) {
-    let sendTimes: number[] = [];
-    let confirmationTimes: number[] = [];
+      let outputData: Data[] = [];
 
-    for (let i = 0; i < 5; i++) {
-      const startSend = Date.now();
-      const transactionParameters = {
-        to: Kyle,
-        value: ethers.utils.parseUnits("0.00001", "ether"),
-        gasPrice: gasPrice,
-        gasLimit: ethers.utils.hexlify(100000),
+      for (let awaitMethod in AwaitMethod) {
+        let sendTimes: number[] = [];
+        let confirmationTimes: number[] = [];
 
-        nonce: await provider.getTransactionCount(wallet.address, "latest"),
-        chainId: 42220,
-      };
-      const tx = await signer.sendTransaction(transactionParameters);
-      const hash = tx.hash;
-      invariant(hash);
-      sendTimes = sendTimes.concat(Date.now() - startSend);
-      const startConfirm = Date.now();
-      if (awaitMethod === AwaitMethod.On) {
-        await new Promise((resolve: (e: ContractReceipt) => void) =>
-          provider.once(hash, (e: ContractReceipt) => {
-            resolve(e);
-            return e;
-          })
+        for (let i = 0; i < 3; i++) {
+          const startSend = Date.now();
+          const transactionParameters = {
+            to: Kyle,
+            value: ethers.utils.parseUnits("0.00001", "ether"),
+            gasPrice: gasPrice,
+            gasLimit: ethers.utils.hexlify(100000),
+
+            nonce: await provider.getTransactionCount(wallet.address, "latest"),
+          };
+          const tx = await signer.sendTransaction(transactionParameters);
+          const hash = tx.hash;
+          invariant(hash);
+          sendTimes = sendTimes.concat(Date.now() - startSend);
+          const startConfirm = Date.now();
+          if (awaitMethod === AwaitMethod.On) {
+            await new Promise((resolve: (e: ContractReceipt) => void) =>
+              provider.once(hash, (e: ContractReceipt) => {
+                resolve(e);
+                return e;
+              })
+            );
+          } else if (awaitMethod === AwaitMethod.Wait) {
+            await tx.wait();
+          } else {
+            console.error("I wish typescript had pattern matching");
+          }
+
+          confirmationTimes = confirmationTimes.concat(
+            Date.now() - startConfirm
+          );
+        }
+
+        const means = [sendTimes, confirmationTimes].map(
+          (times) =>
+            times.reduce((acc, cur) => acc + cur, 0) / (times.length * 1000)
         );
-      } else if (awaitMethod === AwaitMethod.Wait) {
-        await tx.wait();
-      } else {
-        console.error("I wish typescript had pattern matching");
+
+        const data: Data = {
+          awaitMethod: awaitMethod,
+          sendMean: means[0]!,
+          confirmMean: means[1]!,
+          sendRawData: sendTimes,
+          confirmRawData: confirmationTimes,
+        };
+
+        outputData = outputData.concat(data);
       }
+      let out: Output = {
+        endpoint,
+        data: outputData,
+      };
 
-      confirmationTimes = confirmationTimes.concat(Date.now() - startConfirm);
-    }
-
-    const means = [sendTimes, confirmationTimes].map(
-      (times) =>
-        times.reduce((acc, cur) => acc + cur, 0) / (times.length * 1000)
-    );
-
-    const data: Data = {
-      awaitMethod: awaitMethod,
-      sendMean: means[0]!,
-      confirmMean: means[1]!,
-      sendRawData: sendTimes,
-      confirmRawData: confirmationTimes,
-    };
-
-    outputData = outputData.concat(data);
-  }
-  let out: Output = {
-    endpoint,
-    data: outputData,
-  };
-
-  output = output.concat(out);
+      output = output.concat(out);
+    })
+  );
 
   await fs.writeFile("data/times.json", JSON.stringify(output, null, 2));
 };
