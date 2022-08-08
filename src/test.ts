aa6f05dcb5250e30b8a652bed7bcd7f4efd4f8c7
Kyle Scott <kyscott@umich.edu>
2022-08-08 18:59:13 -0400

    test works

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,18 @@
 import { ethers } from "ethers";
+import { ContractReceipt } from "ethers/contract";
 import { load } from "ts-dotenv";
+import invariant from "tiny-invariant";
+
+const endpoints = ["https://forno.celo.org"];
 
 const Kyle = "0x59A6AbC89C158ef88d5872CaB4aC3B08474883D9";
+
+enum AwaitMethod {
+  Wait,
+  On,
+}
+
+const awaitMethod = AwaitMethod.On;
 
 export const testResponseTime = async (): Promise<void> => {
   const provider = new ethers.providers.JsonRpcProvider(
@@ -9,27 +20,54 @@ export const testResponseTime = async (): Promise<void> => {
   );
   const gasPrice = await provider.getGasPrice();
   const env = load({
-    KEY: String,
+    MNEMONIC: String,
   });
-  const wallet = ethers.Wallet.fromMnemonic(
-    "oak few hurt snack habit flame ivory style organ online wild hip"
-  );
+  const wallet = ethers.Wallet.fromMnemonic(env.MNEMONIC);
   const signer = wallet.connect(provider);
 
-  const transactionParameters = {
-    to: Kyle,
-    value: ethers.utils.parseUnits("0.001", "ether"),
-    gasPrice: gasPrice,
-    gasLimit: ethers.utils.hexlify(100000),
+  let sendTimes: number[] = [];
+  let confirmationTimes: number[] = [];
 
-    nonce: await provider.getTransactionCount(wallet.address, "latest"),
-    chainId: 42220,
-  };
+  for (let i = 0; i < 10; i++) {
+    const startSend = Date.now();
+    const transactionParameters = {
+      to: Kyle,
+      value: ethers.utils.parseUnits("0.00001", "ether"),
+      gasPrice: gasPrice,
+      gasLimit: ethers.utils.hexlify(100000),
 
-  const tx = await signer.sendTransaction(transactionParameters);
-  console.log(tx.hash);
-  await tx.wait();
-  console.log("done");
+      nonce: await provider.getTransactionCount(wallet.address, "latest"),
+      chainId: 42220,
+    };
+    const tx = await signer.sendTransaction(transactionParameters);
+    const hash = tx.hash;
+    invariant(hash);
+    sendTimes = sendTimes.concat(Date.now() - startSend);
+    const startConfirm = Date.now();
+    if (awaitMethod === AwaitMethod.On) {
+      await new Promise((resolve: (e: ContractReceipt) => void) =>
+        provider.once(hash, (e: ContractReceipt) => {
+          resolve(e);
+          return e;
+        })
+      );
+    } else if (awaitMethod === AwaitMethod.Wait) {
+      await tx.wait();
+    }
+    confirmationTimes = confirmationTimes.concat(Date.now() - startConfirm);
+  }
+
+  const means = [sendTimes, confirmationTimes].map(
+    (times) => times.reduce((acc, cur) => acc + cur, 0) / (times.length * 1000)
+  );
+
+  console.log("Send Time Mean:", means[0], "all data:", sendTimes);
+  console.log(
+    "Confirmation Time Mean:",
+    means[1],
+    "all data:",
+    confirmationTimes
+  );
 };
 
 testResponseTime().catch((err) => {
